c2eec2dcb3b9f0a8ff8cc1f21a6f3d358bb846dc
Moises Camacho <moisescamacho26@gmail.com>
2021-04-18 21:55:05 -0400

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { LoginComponent } from './examples/login/login.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'index', pathMatch: 'full' },
+    { path: 'index',                component: ComponentsComponent },
+    { path: 'nucleoicons',          component: NucleoiconsComponent },
+    { path: 'examples/landing',     component: LandingComponent },
+    { path: 'examples/login',       component: LoginComponent },
+    { path: 'examples/profile',     component: ProfileComponent },
+    { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+    ],
+})
+export class AppRoutingModule { }
