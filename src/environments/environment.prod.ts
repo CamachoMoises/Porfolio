81b9c8c36ded688126a092138f83b91e843a3154
Moises Camacho <moisescamacho26@gmail.com>
2021-04-20 08:58:19 -0400

    Subiendo a firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCnQaRbFu8chgp6RTxVAIPSpksnLzisJGQ",
+    authDomain: "porfolio-1b48d.firebaseapp.com",
+    projectId: "porfolio-1b48d",
+    storageBucket: "porfolio-1b48d.appspot.com",
+    messagingSenderId: "116904635071",
+    appId: "1:116904635071:web:f5b1752de5900722c3740a",
+    measurementId: "G-RS2RJHBTL8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCnQaRbFu8chgp6RTxVAIPSpksnLzisJGQ",
+    authDomain: "porfolio-1b48d.firebaseapp.com",
+    projectId: "porfolio-1b48d",
+    storageBucket: "porfolio-1b48d.appspot.com",
+    messagingSenderId: "116904635071",
+    appId: "1:116904635071:web:f5b1752de5900722c3740a",
+    measurementId: "G-RS2RJHBTL8"
+  }
 };
